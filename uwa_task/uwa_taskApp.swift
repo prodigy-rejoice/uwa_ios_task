c377ced088fdf8cd4022dc17e27f66c6dbde6ecf
rejoice <omotunwaserejoicerejoice@gmail.com>
2026-09-24 19:13:10 +0100

    Initial Commit

---
+++ uwa_task/uwa_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  uwa_taskApp.swift
+//  uwa_task
+//
+//  Created by macbook on 9/24/26.
+//
+
+import SwiftUI
+
+@main
+struct uwa_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
